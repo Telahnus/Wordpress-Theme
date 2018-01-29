86f5bb9e1c050d7c82e8a238867b6da9e860f0df
Telahnus <quan.jordan@gmail.com>
2018-01-28 22:17:23 -0400

    must upload more frequently

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function theme_styles() {
-    wp_enqueue_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' );
+    // havent needed bootstrap yet
+    //wp_enqueue_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' );
     
     $parent_style = 'twentyseventeen-style';
 
@@ -18,9 +19,37 @@ add_action( 'wp_enqueue_scripts', 'theme_styles');
 
 function theme_js() {
     global $wp_scripts;
-    wp_enqueue_script( 'bootstrap_js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
-	/* wp_enqueue_script( 'my_custom_js', get_template_directory_uri() . '/js/scripts.js'); */
+    // havent needed bootstrap yet
+    //wp_enqueue_script( 'bootstrap_js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
+    /* wp_enqueue_script( 'my_custom_js', get_template_directory_uri() . '/js/scripts.js'); */
+    
+    wp_dequeue_script('twentyseventeen-global');
+    wp_enqueue_script('child-script', get_stylesheet_directory_uri().'/assets/js/global.js', array('jquery'));
+
+
 }
 add_action( 'wp_enqueue_scripts', 'theme_js');
+
+function overwrite_body_class($classes){
+    //$classes[] = 'test-successful';
+    
+    $slug = get_page_template_slug();
+    //error_log($slug);
+    if ($slug=='page-templates/single-column.php'){
+        error_log($slug);
+        $newclasses = null;
+        for ($i=0; $i<count($classes); $i++){
+            if ($classes[$i]!='page-two-column'){
+                //unset($classes[$i]);
+                $newclasses[] = $classes[$i];
+            }
+        }
+        return $newclasses;
+    } else {
+        return $classes;
+    }
+    
+}
+add_filter('body_class', 'overwrite_body_class', 20);
 
 ?>
